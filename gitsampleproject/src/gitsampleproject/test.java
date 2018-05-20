7c31acf7c4503993fcc150f434d47b52a39a326d
anantha kumar <ananthcse502@gmail.com>
2018-05-20 17:14:27 +0530

    commiting project

---
+++ gitsampleproject/src/gitsampleproject/test.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package gitsampleproject;
+
+/**
+ * @author ananth
+ *
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello");
+	}
+
+}
